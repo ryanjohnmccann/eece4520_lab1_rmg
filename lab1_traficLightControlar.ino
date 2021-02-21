0acfea7b4e66a9e03a30def42472dba15ae5a8eb
someone <someone@sameplace.com>
2021-02-21 00:10:46 -0500

    Update lab1_traficLightControlar.ino

--- lab1_traficLightControlar.ino
+++ lab1_traficLightControlar.ino
@@ -1,9 +1,9 @@
-int yellowpin = 25;
-int redpin = 23;
-int greenpin =24;
-int buzz =  45;
-int button = 35;
-int buttonpress = 0;
+int yellowpin = 4;
+int redpin = 6;
+int greenpin =5;
+int buzz =  12;
+int button = 10;
+bool  buttonpress = true;
 
 
 void setup() {
@@ -11,45 +11,41 @@ void setup() {
   pinMode(greenpin,OUTPUT);
   pinMode(redpin,OUTPUT);
   pinMode(buzz,OUTPUT);
-  pinMode(button,INPUT);
+  pinMode(button,INPUT_PULLUP);
   
 }
 
+void toggle_led(int Pin, int delay1){
+  for(delay1 ; delay1 >=0; delay1--){
+    digitalWrite(Pin,HIGH);
+    delay(1);
+    if(delay1 == 3000){
+      digitalWrite(buzz,HIGH);
+      delay(100);
+      digitalWrite(buzz,LOW);
+    }
+  }
+  digitalWrite(Pin,LOW);
+}
+
 void loop() {
-  while(buttonpress ==0){
-    if(digitalRead(button) == HIGH){
-      buttonpress = 1;
+  while(buttonpress){
+    if(digitalRead(button) == LOW){
+      buttonpress = false;
+      break;  
     }
     digitalWrite(redpin,HIGH);
-    delay(1000);
+    delay(100);
     digitalWrite(redpin,LOW);
-    delay(1000);
-  }  
+    delay(100);
+  }
  
-  for(int Pin = 23; Pin <= 25 ; Pin++){
-      if( Pin == 25){
-        digitalWrite(Pin, HIGH);
-        digitalWrite(buzz,HIGH);
-        delay(1000);
-        digitalWrite(buzz,LOW);
-      
-        delay(2000);
-        digitalWrite(Pin,LOW);
-       
-       
+  for(int Pin = 6 ; Pin >= 4 ; Pin--){
+      if( Pin == 4){ 
+       toggle_led(Pin, 3000);
       }
       else{
-        digitalWrite(Pin, HIGH);
-        delay(12000);
-     
-        digitalWrite(buzz,HIGH);
-        delay(1000);
-        digitalWrite(buzz,LOW);
-
-        delay(2000);
-        digitalWrite(Pin,LOW);
-     
-        
+        toggle_led(Pin, 15000);        
       } 
   }
 }
