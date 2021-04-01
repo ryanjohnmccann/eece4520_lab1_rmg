503722840d3c96d3d31df6583097b8a8d5c93f4e
someone <someone@sameplace.com>
2021-04-01 19:58:24 -0400

    Update lab3.ino
    update

--- lab3/lab3.ino
+++ lab3/lab3.ino
@@ -1,78 +1,80 @@
 
-#define ENABLE 5
-#define DIRA 3
-#define DIRB 4
-#define button 2
+const int ENABLE = 5; // connected to enable pin 1 of the L293D
+const int DIRA  = 6; // connected to In pin 1 of the L293D
+const int DIRB  = 4; // connected to In pin 2 of the L293D
+const int button = 2; // connected to the interupt for button press detection
 
-int buttonpress = 0;
-int s;
-int maxspeed = 255;
+volatile bool state = LOW; // if state is low it is rotation of motor CC if high it is rotation of motor is C
+const double maxspeed = 255; //max speed of the PMW at enable pin of the L293D form 0 - 255
+int speed1; //indicated the speed/PMW for the enable pin
 
+//set up pin output, input, and ISR
 void setup() {
-  //---set pin direction
   pinMode(ENABLE,OUTPUT);
   pinMode(DIRA,OUTPUT);
   pinMode(DIRB,OUTPUT);
   pinMode(button, INPUT_PULLUP);
-  Serial.begin(9600);
+  attachInterrupt(digitalPinToInterrupt(button), rotate, FALLING);
+  
+  digitalWrite(DIRA,state); 
+  digitalWrite(DIRB,state);
 }
 
+//controles speed and direction of the motor
 void speed(int sp){
   analogWrite(ENABLE,sp);
+  digitalWrite(DIRA,state); 
+  digitalWrite(DIRB,!state);
+  delay(2000);              
 }
 
-void rotate(int side){
-  
-  if(side == 1){
-    digitalWrite(DIRA,HIGH); //right
-    digitalWrite(DIRB,LOW); 
+//ISR to change rotation of motor
+void rotate(){
+  state = !state;
+}
+
+//speed up the motor at incriments of 2/4, 3/4, and full of max speed for motor
+void speedup(){
+  for(double i = 63.75; i <= 255; i = i + (maxspeed*0.25)){
+    if( i == 63.75){
+      i=i+maxspeed*0.25; 
+      speed1 = i;
+    }else{
+      speed1 = i;
+    }
+    speed(speed1); 
   }
-  if(side == 0){
-    digitalWrite(DIRA,LOW); //left
-    digitalWrite(DIRB,HIGH);   
-  } 
-    
 }
 
+//slow down the motor at incriments of 3/4,1/2,0 of max speed of motor
+void slowdown(){
+  for(double i = 191.25; i >= 0; i = i - (maxspeed*0.25)){
+    if( i == 63.75){
+      i=i-maxspeed*0.25; 
+      speed1 = i;
+    }else{
+      speed1 = i;
+    }
+    speed(speed1);  
+  }
+}
+
+//main loop toggles between slowing down and speeding up
 void loop() {
 
-  for(int i = 255; i > 0; i = i - (maxspeed*0.25)){
-    if( i == 63){
-      s = 0;
-    }else{
-      s = i;
-    }
-    if(digitalRead(button) == LOW){
-      if(buttonpress == 1){
-        buttonpress = 0;
-      }else{
-         buttonpress =1;
-      }
-      s = 255; 
-    }
-    speed(s);
-    rotate(buttonpress);
-    delay(1000);
-    Serial.print("Button = ");
-    Serial.print(digitalRead(button));
-    Serial.println(); 
-    Serial.print("Buttonpress = ");
-    Serial.print(buttonpress);
-    Serial.println(); 
-    Serial.print("Speed = ");
-    Serial.print(s);
-    Serial.println(); 
-    Serial.print("I = ");
-    Serial.print(i);
-    
-    Serial.println();
-    Serial.println();  
+  if(speed1 == 0){
+    speedup(); 
   }
+  else if(speed1 = 255){
+    slowdown();     
+  }
+  
+}
   
   
     
   
 
     
-}
+
    
