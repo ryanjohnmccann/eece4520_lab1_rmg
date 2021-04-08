74ba16a928433cc0ccea4eb866fdb321fefe3bc6
someone <someone@sameplace.com>
2021-04-08 10:07:52 -0400

    update
    changed ENABLE to pin 3
    changed LCD pin please compare to yours
    Over all using 3 different interrupts to handal each element of the lab

--- lab3/lab3.ino
+++ lab3/lab3.ino
@@ -1,75 +1,88 @@
-//Library for LCD
+#include <Wire.h>
 #include <LiquidCrystal.h>
-
-
+#include <TimeLib.h>
+#include <DS1307RTC.h>
 
 //pins for motor
-const int ENABLE = 5; // connected to enable pin 1 of the L293D
+const int ENABLE = 3; // connected to enable pin 1 of the L293D
 const int DIRA  = 6; // connected to In pin 1 (pin2) of the L293D
 const int DIRB  = 4; // connected to In pin 2 (pin8) of the L293D
-const int button = 2; // connected to the interupt for button press detection
+const int button = 2; //button pin
 
-bool state = LOW; // if state is low it is rotation of motor CC if high it is rotation of motor is C
 double speed1[4] = {0,127.5,191.25,255}; //indicated the speed/PMW for the enable pin
+volatile bool state = LOW; // starts direction of CC
+int i = 0; // used to move within speed1 to change speed
 
 //Pins for LCD displacy
-const int rs = 12, en =11, d4 = 7, d5 = 8, d6 =9, d7 = 10;
+const int rs = 7, en = 8, d4 = 9, d5 = 10, d6 =11, d7 = 12;
 LiquidCrystal lcd(rs,en,d4,d5,d6,d7);
- 
+
+//used with timer1 to update LCD
+volatile bool update_lcd; 
+
+//used with timer4 to count every timerperiod#
+int TIMEPERIOD = 11;
+volatile int counter = 0;
 
 //set up pin output, input, ISR, and RCT 
 void setup() {
+  lcd.begin(16,2);
+  Serial.begin(9600);
   pinMode(ENABLE,OUTPUT);
   pinMode(DIRA,OUTPUT);
   pinMode(DIRB,OUTPUT);
   pinMode(button, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(button), rotate, FALLING);
   digitalWrite(DIRA,state); 
   digitalWrite(DIRB,state);
+  
+  //Timer intrupte for LCD display every 1 sec
+  noInterrupts();
+  TCCR1A = 0;   //initialized timer1
+  TCCR1B =0;
+  TCCR1B |= 0b00000100; //256 prescaler
+  TIMSK1 |= 0b00000001; //enable overflow interupt
+  interrupts();
 
-  lcd.begin(16,2);
+  //Timer interupte to update speed
+  setupTimer4();
 
+  //button intrupte
+  attachInterrupt(digitalPinToInterrupt(button), rotate, FALLING);
+  
 }
 
-//controles speed and direction of the motor
-void speed(int sp){
-  analogWrite(ENABLE,sp);            
+//timer interupt to incriment speed 
+//runs every 1 second, use count loop to increase/decrease time
+void setupTimer4(){
+  noInterrupts();
+  // Clear registers
+  TCCR4A = 0;
+  TCCR4B = 0;
+  TCCR4B |= 0b00000100;
+  TIMSK4 |= 0b00000001; 
+  interrupts();
 }
 
-//ISR to change rotation of motor
+//button interupte to set state to change direction
+//runs on timer0
 void rotate(){
   state = !state;
-  direction();
-  displayrotation();
-}
-//indicated direction of motor
-void direction(){
-  digitalWrite(DIRA,state); 
-  digitalWrite(DIRB,!state);
-}
-//speed up the motor at incriments of 2/4, 3/4, and full of max speed for motor
-void speedup(){
-  for(int i = 0; i <= 3 ; i++){
-    speed(speed1[i]); 
-    direction();
-    displayspeed(speed1[i]);
-    displayrotation();
-    delay(2000); 
-  }
 }
 
-//slow down the motor at incriments of 3/4,1/2,0 of max speed of motor
-void slowdown(){
-  for(int i = 3; i >= 0; i--){
-    speed(speed1[i]); 
-    direction();
-    displayspeed(speed1[i]);
-    displayrotation();
-    delay(2000); 
-  }
+//interupt to update desplay every 1 sec
+//runs on timer1
+ISR(TIMER1_OVF_vect){
+  update_lcd =true;
 }
+
+//interupt to update speed
+//runns on timer 4
+ISR(TIMER4_OVF_vect){
+   counter = counter +1;
+}
+
 //displays speed
-void displayspeed(double dis){
+void display(double dis){
   lcd.setCursor(0,0);
   lcd.print("Speed:");
   lcd.setCursor(7,0);
@@ -77,9 +90,6 @@ void displayspeed(double dis){
   if(dis == 0.00){
     lcd.print("  ");
   }
-}
-//displays direction
-void displayrotation(){
   lcd.setCursor(14,0);
   if(state == LOW){
     lcd.print("CC");
@@ -87,19 +97,35 @@ void displayrotation(){
   if(state == HIGH){
     lcd.print("C ");
   }
+  lcd.setCursor(0,1);
+  lcd.print(hour());
+  lcd.print(":");
+  lcd.print(minute());
+  lcd.print(":");
+  lcd.print(second());
 }
+
 
 //main loop toggles between slowing down and speeding up
-void loop() { 
-  speedup();
-  slowdown();
-  
-}
-  
-  
-    
-  
-
-    
-
+void loop() {
+   //updates LCD when it s true every 1sec
+   if(update_lcd == true){
+    update_lcd = false;
+    display(speed1[i]);
+   }
+   //changes speed every time it reaches timeperiod value set
+   //counter in incrimented every 1 sec with timer4
+   if(counter >= TIMEPERIOD){
+     i = i+1;
+     if(i == 4){
+       i = 0;
+     }
+     counter = 0;
+   }
    
+   analogWrite(ENABLE,speed1[i]); //changes speed of motor uses timer3 since on pin3
+   //indicates direction of motor when state is low it is CC and high it is C
+   digitalWrite(DIRA,state); 
+   digitalWrite(DIRB,!state);
+ 
+}
